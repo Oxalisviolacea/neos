7bbd5885fc2ccc9e18b8ab35254ccaecdfa5e601
Hanna Davis <hanna.e.davis@gmail.com>
2020-11-30 22:11:01 -0700

    Add tests for largest diameter and total number

--- near_earth_objects_test.rb
+++ near_earth_objects_test.rb
@@ -4,9 +4,19 @@ require 'pry'
 require_relative 'near_earth_objects'
 
 class NearEarthObjectsTest < Minitest::Test
-  def test_a_date_returns_a_list_of_neos
+  def setup
     near_earth_objects = NearEarthObjects.new('2019-03-30')
-    results = near_earth_objects.find_neos_by_date
-    assert_equal '(2011 GE3)', results[:astroid_list][0][:name]
+    @results = near_earth_objects.find_neos_by_date
+  end
+  def test_a_date_returns_a_list_of_neos
+    assert_equal '(2011 GE3)', @results[:asteroid_list][0][:name]
+  end
+
+  def test_find_the_largest_asteroid
+    assert_equal 537, @results[:biggest_asteroid]
+  end
+
+  def test_find_the_total_number_of_asteroids
+    assert_equal 10, @results[:total_number_of_asteroids]
   end
 end
